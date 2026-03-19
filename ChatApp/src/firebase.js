1cba85e1f9bc0bce28d8ec1af947570d4ea1641a
Kaushal Singh Thakur <kaushal21gs@gamil.com>
2026-03-19 13:42:28 +0530

    Good to go

--- ChatApp/src/firebase.js
+++ ChatApp/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3Ct5MXfUuSJLGfd0vwW9dnx3JwAf3mV8",
-    authDomain: "test-learn-73ba9.firebaseapp.com",
-    projectId: "test-learn-73ba9",
-    storageBucket: "test-learn-73ba9.firebasestorage.app",
-    messagingSenderId: "700456625903",
-    appId: "1:700456625903:web:82e9f3dc5436f337d29d1a",
-    measurementId: "G-C7Y7P86Q7B"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 };
 
 const app = initializeApp(firebaseConfig);
